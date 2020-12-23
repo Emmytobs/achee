964b54f11ca1fa8c43e6e87aadb2b56cfc09b4c3
Emmytobs <eotobo11@gmail.com>
2020-12-23 13:45:56 +0100

    Added a new logo and made some changes to the style in small screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import "./Header.css"
 import Icon from "../Icon.js";
-import acheeLogo from "./achee1~2.png";
-import overlay from "./Ellipse 14.svg";
+import acheeLogo from "./acheeLogo.png";
+import overlay_big from "./Ellipse_big.svg";
+import overlay_small from "./Ellipse_small.svg";
 
 function Header() {
     return (
         <header>
             <nav>
-                <div class="logo"><img src={acheeLogo} alt="Achee" /></div>
+                <div class="logo"><img src={acheeLogo} alt="Achee" /> achee</div>
                 <button class="join-waitlist-btn">Join the waitlist <Icon color="#fff" icon="arrow-right" addMargin/></button>
             </nav>
             <section id="landing-page" class="landing-page">
-                <img src={overlay} class="img-overlay" alt="Overlay" />
+                <img src={overlay_big} class="img-overlay" alt="Overlay" />
+                {/* <img src={overlay_small} class="img-overlay" alt="Overlay" /> */}
                 <h1 className="headline">All Your Investments In One Place</h1>
                 <p className="">Monitor stocks, dividends, cryptocurrencies and other fixed income investments. With Achee, you stay on top of your money.</p>
                 <form action="">
