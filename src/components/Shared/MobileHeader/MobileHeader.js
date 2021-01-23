2970d22faca1f76800ec1c4ece3d77a843876ef8
Emmytobs <eotobo11@gmail.com>
2021-01-23 21:19:20 +0100

    Implemented mobile design for the login, signup, reset password, and forgot password pages

--- src/components/Shared/MobileHeader/MobileHeader.js
+++ src/components/Shared/MobileHeader/MobileHeader.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MobileHeader.module.css';
 import menuIcon from '../../Shared/icons/menu-icon.png';
 import acheeLogo from '../../Shared/icons/acheeLogo_blue.png'
+import acheeLogoWhite from '../../Shared/icons/acheeLogo_white.png'
+import closeMenuIcon from '../../Shared/icons/close-menu-icon.png'
 function MobileHeader() {
+    const [isMenuShowing, setIsMenuShowing] = useState(false)
+    const toggleShowMenu = (e) => {
+        setIsMenuShowing(!isMenuShowing)
+    }
+
     return (
-        <header className={`display-flex ${styles.mobileHeader}`}>
-            <div className={`display-flex ${styles.mobileLogo}`}>
-                <img src={acheeLogo} alt="Achee" width="16px" height="16px" />
-                <span>achee</span>
+        <header style={{ backgroundColor: isMenuShowing ? "#805CF5" : "#fff"}} className={`${styles.mobileHeader}`}>
+            <div className={styles.menuBar}>
+                <div className={`display-flex ${styles.mobileLogo}`}>
+                    <img src={isMenuShowing ? acheeLogoWhite : acheeLogo} alt="Achee" width="16px" height="16px" />
+                    <span style={{ color: isMenuShowing && "#fff"}}>achee</span>
+                </div>
+                <button className={styles.openMenuBtn} onClick={toggleShowMenu}>
+                    {isMenuShowing ? <img src={closeMenuIcon} alt="Close Menu Icon" width="20px" height="20px" /> : <img src={menuIcon} alt="Menu Icon" />}
+                </button>
             </div>
-            <div className={styles.menu}>
-                <img src={menuIcon} alt="Menu Icon"/>
+
+            <div style={{ left: isMenuShowing ? "0" : "100%" }} className={styles.menuContainer}>
+                
             </div>
         </header>
     )
