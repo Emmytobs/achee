5722d17056c90ff4142b1f2b14e87ce60b6b02cc
Emmytobs <eotobo11@gmail.com>
2021-01-23 22:09:42 +0100

    Changed the navbar

--- src/components/Shared/MobileHeader/MobileHeader.js
+++ src/components/Shared/MobileHeader/MobileHeader.js
@@ -1,30 +1,55 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './MobileHeader.module.css';
+
 import menuIcon from '../../Shared/icons/menu-icon.png';
 import acheeLogo from '../../Shared/icons/acheeLogo_blue.png'
 import acheeLogoWhite from '../../Shared/icons/acheeLogo_white.png'
 import closeMenuIcon from '../../Shared/icons/close-menu-icon.png'
 function MobileHeader() {
-    const [isMenuShowing, setIsMenuShowing] = useState(false)
+    const [isMenuShowing, setIsMenuShowing] = useState(true)
     const toggleShowMenu = (e) => {
         setIsMenuShowing(!isMenuShowing)
     }
 
     return (
-        <header style={{ backgroundColor: isMenuShowing ? "#805CF5" : "#fff"}} className={`${styles.mobileHeader}`}>
+        <header className={`${styles.mobileHeader}`}>
             <div className={styles.menuBar}>
                 <div className={`display-flex ${styles.mobileLogo}`}>
-                    <img src={isMenuShowing ? acheeLogoWhite : acheeLogo} alt="Achee" width="16px" height="16px" />
-                    <span style={{ color: isMenuShowing && "#fff"}}>achee</span>
+                    <img src={acheeLogo} alt="Achee" width="16px" height="16px" />
+                    <span>achee</span>
                 </div>
                 <button className={styles.openMenuBtn} onClick={toggleShowMenu}>
-                    {isMenuShowing ? <img src={closeMenuIcon} alt="Close Menu Icon" width="20px" height="20px" /> : <img src={menuIcon} alt="Menu Icon" />}
+                    <img src={menuIcon} alt="Menu Icon" />
                 </button>
             </div>
 
-            <div style={{ left: isMenuShowing ? "0" : "100%" }} className={styles.menuContainer}>
-                
+            {/* Menu Items Container */}
+            <div
+                style={{ left: isMenuShowing ? 0 : '100%', visibility: isMenuShowing ? 'visible' : 'hidden' }}
+                className={styles.menuContainer}>
+                <div className={styles.menuBar}>
+                    <div className={`display-flex ${styles.mobileLogo}`}>
+                        <img src={acheeLogoWhite} alt="Achee" width="16px" height="16px" />
+                        <span style={{ color: "#fff"}}>achee</span>
+                    </div>
+                    <button className={styles.closeMenuBtn} onClick={toggleShowMenu}>
+                        <img src={closeMenuIcon} alt="Close Menu Icon" width="20px" height="20px" />
+                    </button>
+                </div>
+                <div className={styles.menuItems}>
+                    <ul>
+                        <li><Link>Home</Link></li>
+                        <li><Link>Portfolio</Link></li>
+                        <li><Link>Learn</Link></li>
+                        <li><Link>Contact Us</Link></li>
+                        <li><Link>My Profile</Link></li>
+                        <li><Link>Upgrade</Link></li>
+                        <li><Link>Sign out</Link></li>
+                    </ul>
+                </div>
             </div>
+            {/* } */}
         </header>
     )
 }
