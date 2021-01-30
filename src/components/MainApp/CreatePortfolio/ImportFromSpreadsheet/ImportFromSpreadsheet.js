22e54550f5c5198ec359f9c0a387b64b8f14ebfe
Emmytobs <eotobo11@gmail.com>
2021-01-30 21:53:53 +0100

    Desgined the import from spreadshett page

--- src/components/MainApp/CreatePortfolio/ImportFromSpreadsheet/ImportFromSpreadsheet.js
+++ src/components/MainApp/CreatePortfolio/ImportFromSpreadsheet/ImportFromSpreadsheet.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { PrimaryButton } from '../../../Shared/Utilities'
+
 import styles from './ImportFromSpreadsheet.module.css'
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer'
+
+import importIcon from './import.png'
 
 function ImportFromSpreadsheet(props) {
     return (
         <>
         <Header {...props} />
-        <div> className={styles.container}
+        <div className={'display-flex '+ styles.container}>
             <div>
-                <h2>Import Portfolio from Spreadsheet</h2>
+                <h2 className={styles.pageTitle}>Import Portfolio from Spreadsheet</h2>
                 <div className={styles.importPortfolioContainer}>
-                    <p>By clicking on choose files you would be able to Import your exisiting portfolios. </p>
+                    <h3>By clicking on choose files you would be able to Import your exisiting portfolios. </h3>
                     <div className={'display-flex '}>
-                        <img src="" alt="Import Portfolio"/>
-                        <div>
-                            <p>Random content</p>
+                        <div className={'display-flex '+ styles.imgContainer}>
+                            <img src={importIcon} alt="Import Portfolio" width='120px' height='120px' />
+                        </div>
+                        <div className={styles.uploadCSV}>
+                            <p>Select CSV files</p>
+                            <input type="file" />
+                            <PrimaryButton style={{
+                                padding: '15px 40px'
+                            }}>Import Portfolio</PrimaryButton>
                         </div>
                     </div>
 
