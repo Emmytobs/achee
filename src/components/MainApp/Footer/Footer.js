564889d44a9eca803e240cf5654965d990e24182
Emmytobs <eotobo11@gmail.com>
2021-01-30 12:58:55 +0100

    Refactored footer design to use CSS grid

--- src/components/MainApp/Footer/Footer.js
+++ src/components/MainApp/Footer/Footer.js
@@ -12,8 +12,9 @@ function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <footer className={`${styles.footer}`}>
-            <div className={`display-flex ${styles.firstRow}`}>
-                <div className={`display-flex  ${styles.logo}`}>
+            <div className={styles.gridContainer}>
+                
+                <div className={`${styles.logo}`}>
                     <img src={acheeLogo} alt="Achee logo" width="16px" height="16px"/>
                     <span>achee</span>
                 </div>
@@ -21,8 +22,7 @@ function Footer() {
                 <div className={styles.btnContainer}>
                     <Button buttonText="Click to Update" style={{ background: 'rgba(0,0,0,0.2)', margin: '0'}} />
                 </div>
-            </div>
-            <div className={`display-flex ${styles.footerMenuContainer}`}>
+
                 <div className={`${styles.menuList} ${styles.sitemap}`}>
                     <h3>Sitemap</h3>
                     <ul>
@@ -47,7 +47,7 @@ function Footer() {
                         <li><Link>Contact Us</Link></li>
                     </ul>
                 </div>
-                <div className={styles.connect}>
+                <div className={styles.socialMedia}>
                     <h3>Social Media</h3>
                     <div>
                         <p>Follow us on social media to find out the latest updates on our progress</p>
@@ -58,6 +58,7 @@ function Footer() {
                         </div>
                     </div>
                 </div>
+            
             </div>
             <small>
                 Achee @ {currentYear}. All Rights Reserved
