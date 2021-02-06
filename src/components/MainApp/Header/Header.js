3487e6be91060fa3e9f5e62d55cdb25e9cbda25c
Emmytobs <eotobo11@gmail.com>
2021-02-06 19:25:32 +0100

    Fixed the tab indicator issue on the header; made some other changes

--- src/components/MainApp/Header/Header.js
+++ src/components/MainApp/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom'
 
 import styles from './Header.module.css'
 
@@ -11,20 +11,14 @@ import userDP from '../../Shared/icons/user-dp.png';
 import MobileHeader from '../../Shared/MobileHeader/MobileHeader';
 
 function Header(props) {
-    const [activeTab, setActiveTab] = useState('home');
-
-    const changeTab = (tab) => {
-        setActiveTab(tab);
-        console.log(tab)
+    const { url } = useRouteMatch()
+    const routeIsInUrl = (route, exact) => {
+        if (exact) {
+            return url === route
+        }
+        return url.includes(route);
     }
-
-    const takeTo = (path, active) => {
-        changeTab(active)
-        if(path) {
-            props.history.push(path);
-        } 
-    }
-
+   
     return (
         <div className={styles.headerContainer}>
             <header className={`display-flex ${styles.header}`}>
@@ -33,24 +27,30 @@ function Header(props) {
                     <span>achee</span>
                 </div>
                 <nav className={`display-flex ${styles.navbar}`}>
-                    <Link className={styles.tab} onClick={takeTo.bind(null, '/app', 'home')}>
+                    <Link className={styles.tab} to="/app">
                         Home
-                        {activeTab === "home" && <span className={styles.indicator}></span>}
+                        {routeIsInUrl('/app', 'exact') && <span className={styles.indicator}></span>}
                     </Link>
-                    <Link className={styles.tab} onClick={takeTo.bind(null, '/app/create-portfolio', 'portfolio')}>
+                    
+                    <Link className={styles.tab} to="/app/create-portfolio">
                         Portfolio
-                        {activeTab === "portfolio" && <span className={styles.indicator}></span>}
+                        {routeIsInUrl('/create-portfolio') && <span className={styles.indicator}></span>}
                     </Link>
-                    <Link onClick={takeTo.bind(null, null, 'resources')} className={`${styles.tab} ${styles.resourcesTab}`}>
+
+                    <Link className={`${styles.tab} ${styles.resourcesTab}`}>
                         Resources <img src={arrowDown} alt="Arrow Down"/>
-                        {activeTab === "resources" && <span className={styles.indicator}></span>}
+                        {routeIsInUrl('/resources') && <span className={styles.indicator}></span>}
                     </Link>
-                    <Link onClick={takeTo.bind(null, '/app/referrals', 'referrals')} className={styles.tab} to="">
+
+                    <Link className={styles.tab} to="/app/referrals">
                         Refer a Friend
-                        {activeTab === "referrals" && <span className={styles.indicator}></span>}
+                        {routeIsInUrl('/referrals') && <span className={styles.indicator}></span>}
                     </Link>
+
                     <Link><button className={styles.upgradeButton}>Upgrade</button></Link>
+
                     <Link><img src={notificationIcon} alt="Notification" /></Link>
+
                     <div className={`display-flex ${styles.dropdownBtn}`}>
                         <img src={userDP} alt="User's Profile Pic" />
                         <p className={styles.userFullName}>Adesanoye Dorcas</p>
