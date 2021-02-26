0c56703d731455310b258e6fe97cc98b7fcd44ec
Emmytobs <eotobo11@gmail.com>
2021-02-26 08:48:19 +0100

    Refactored the no-internet page also; Added a little margin to the bottom of the illustration on the 404 page

--- src/components/NoInternet/NoInternet.js
+++ src/components/NoInternet/NoInternet.js
@@ -14,7 +14,8 @@ function NoInternet() {
                     <span>achee</span>
                 </div>
                 <div className={"display-flex justify-between align-center " + styles.flexContainer}>
-                    <div>
+                    <img src={noInternetIllustration} alt="No Internet Illustration" width="547px" height="427px" />
+                    <div className={styles.helpText}>
                         <h1>No Internet Connection.</h1>
                         <p>Please check your connection again or connect to Wi-Fi.</p>
                         <div className={"display-flex justify-start align-center"}>
@@ -22,7 +23,6 @@ function NoInternet() {
                             <Button style={{ backgroundColor: '#F3EFFF', marginLeft: "20px" }}>Go to homepage</Button>
                         </div>
                     </div>
-                    <img src={noInternetIllustration} alt="No Internet Illustration" width="547px" height="427px" />
                 </div>
             </div>
         </div>
